cec636e23eb23a4d17e6bbfb693cb638293165e6
Mengnan Li <mengnanli91@gmail.com>
2024-08-02 13:41:36 -0600

    Add input files and python scripts for model verification, update validation script and result plots

---
+++ modules/navier_stokes/test/tests/finite_volume/two_phase/mixture_model/model_verification/verification_moment.py
@@ -0,0 +1,91 @@
+# The mixture model verification
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+import os.path
+
+def read_data(file_name):
+
+  my_path = os.path.abspath(os.path.dirname(__file__))
+  path = os.path.join(my_path, file_name)
+
+  MASS_data = pd.read_csv(path)
+
+  return MASS_data
+
+def analytical_solution_vel(data):
+  analytical_data = pd.DataFrame(data['time'])
+  analytical_data['outlet_vel'] = 1.992-0.9921*np.cos(np.pi*analytical_data['time'])
+
+  return analytical_data
+
+def plot_vel(data, label, marker='o', linestyle='none'):
+  plt.plot(data['time'], data['outlet_vel'], label=label,linestyle=linestyle, marker=marker)
+
+def plot_norm(x, y, order, label, marker='x', linestyle='none'):
+
+  xx = np.linspace(np.min(x), np.max(x), 30)
+  if order == '1':
+    a = y[0]/x[0]
+    plt.loglog(xx, a*xx, label='first order', linestyle=':')
+  if order == '2':
+    a = y[0]/(x[0]*x[0])
+    plt.loglog(xx, a*xx*xx, label='second order', linestyle=':')
+  plt.loglog(x,y, label=label,linestyle=linestyle, marker=marker)
+
+def L2_norm(data, analytical):
+  difference = data - analytical
+  l2_norm = np.linalg.norm(difference)
+  l_infinite = np.max(np.abs(difference))
+  return l2_norm, l_infinite
+
+
+
+def main():
+  file1 = "./moment_dt=0.05.csv"
+  file2 = "./moment_dt=0.025.csv"
+  file3 = "./moment_dt=0.0125.csv"
+
+  data1 = read_data(file1)
+  data2 = read_data(file2)
+  data3 = read_data(file3)
+  ana_data = analytical_solution_vel(data1)
+
+  plot_vel(data1, 'MOOSE')
+  plot_vel(ana_data, 'Analytical', linestyle='-', marker='none')
+
+  plt.title('Momentum Transient')
+  plt.xlabel('time(s)')
+  plt.ylabel('velocity($m/s$)')
+  plt.legend(loc='best')
+  plt.savefig('results/MomentumTransient.png', dpi=200)
+  plt.show()
+
+  dt = [0.05, 0.025, 0.0125]
+  L2_1, Lini_1 = L2_norm(data1['outlet_vel'], analytical_solution_vel(data1)['outlet_vel'])
+  L2_2, Lini_2= L2_norm(data2['outlet_vel'], analytical_solution_vel(data2)['outlet_vel'])
+  L2_3, Lini_3 = L2_norm(data3['outlet_vel'], analytical_solution_vel(data3)['outlet_vel'])
+
+  L2_norm_vel = []
+  L2_norm_vel.append(L2_1)
+  L2_norm_vel.append(L2_2)
+  L2_norm_vel.append(L2_3)
+
+  Linfini_norm_vel = []
+  Linfini_norm_vel.append(Lini_1)
+  Linfini_norm_vel.append(Lini_2)
+  Linfini_norm_vel.append(Lini_3)
+
+  plot_norm(dt, L2_norm_vel, order='1', label='$l_2$', marker='x', linestyle='none')
+  plot_norm(dt, Linfini_norm_vel, order='1', label='$l_{\infty}$', marker='x', linestyle='none')
+
+  plt.title('Momentum Transient Convergence')
+  plt.xlabel('Timestep size(s)')
+  plt.ylabel('$l_2$ error of velocity [$m/s$]')
+  plt.legend(loc='best')
+  plt.savefig('results/MomentumTransientConvergence.png', dpi=200)
+  plt.show()
+
+if __name__ == "__main__":
+    main()
--- modules/navier_stokes/test/tests/finite_volume/two_phase/mixture_model/modelvalidation.py
+++ modules/navier_stokes/test/tests/finite_volume/two_phase/mixture_model/modelvalidation.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-def plot_data_and_relative_error(experimental_df, numerical_df, label, dpi):
+def plot_data_and_relative_error(experimental_df, numerical_df, labels, dpi):
     # Ensure that the dataframes have the same index and columns
     if not experimental_df.index.equals(numerical_df.index) or not experimental_df.columns.equals(numerical_df.columns):
         raise ValueError("Experimental and numerical dataframes must have the same index and columns")
@@ -15,36 +15,42 @@ def plot_data_and_relative_error(experimental_df, numerical_df, label, dpi):
     # Set global font size
     plt.rcParams.update({'font.size': 14})
 
-    # Plot experimental vs numerical data
-    plt.figure(figsize=(12, 6))
+    for label in labels:
+        # Plot experimental vs numerical data
+        plt.figure(figsize=(12, 6))
 
-    plt.plot(experimental_df['void_fraction'], experimental_df['vg'], '-o', label=label)
-    plt.plot(numerical_df['void_fraction'], numerical_df['vg'], '--x', label='MOOSE')
+        plt.plot(experimental_df[f'void_fraction_{label}'], experimental_df[f'vg_{label}'], '-o', label=label, markersize=12, linewidth=3, markerfacecolor='none')
+        plt.plot(numerical_df[f'void_fraction_{label}'], numerical_df[f'vg_{label}'], '--s', label='MOOSE', markersize=12, linewidth=3, markerfacecolor='none')
 
-    plt.xlabel('Void Fraction')
-    plt.ylabel('Gas velocity(m/s)')
-    plt.ylim([0,1])
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(os.path.join('results/', f'gas_velocity_{label}.png'), dpi=dpi)
-
-    # Plot relative error
-    plt.figure(figsize=(12, 6))
+        plt.xlabel(r'Void Fraction[-]')
+        plt.ylabel(r'Gas velocity($\mathrm{m/s}$)')
+        plt.ylim([0,1])
+        plt.legend()
+        plt.grid(True)
+        plt.savefig(os.path.join('results/', f'gas_velocity_{label}.png'), dpi=dpi)
 
 
-    plt.plot(experimental_df['void_fraction'], relative_error_df['vg'],'-o')
+    for label in labels:
+        # Plot relative error
+        plt.figure(figsize=(12, 6))
 
-    plt.xlabel('Void Fraction')
-    plt.ylabel('Relative Error')
-    plt.ylim([0,0.01])
-    plt.grid(True)
-    plt.savefig(os.path.join('results/', f'relative_error_{label}.png'), dpi=dpi)
+        plt.plot(experimental_df[f'void_fraction_{label}'], relative_error_df[f'vg_{label}'],'-o')
+
+        plt.xlabel('Void Fraction')
+        plt.ylabel(f'Relative Error for {label}')
+        plt.grid(True)
+        plt.savefig(os.path.join('results/', f'relative_error_{label}.png'), dpi=dpi)
 
 # Bhagwat2016
-experimental_data = {'void_fraction': [0.455, 0.475, 0.55, 0.6], 'vg': [0.21, 0.58, 0.42, 0.65]}
-numerical_data = {'void_fraction': [0.455, 0.475, 0.55, 0.6], 'vg': [0.2095, 0.5804, 0.4176, 0.6480]}
+experimental_data = {'void_fraction_Bhagwat2016': [0.455, 0.475, 0.55, 0.6], 'vg_Bhagwat2016': [0.21, 0.58, 0.42, 0.65]}
+numerical_data = {'void_fraction_Bhagwat2016': [0.455, 0.475, 0.55, 0.6], 'vg_Bhagwat2016': [0.2095, 0.5804, 0.4176, 0.6480]}
 experimental_df = pd.DataFrame(experimental_data)
 numerical_df = pd.DataFrame(numerical_data)
 
-plot_data_and_relative_error(experimental_df, numerical_df, 'Bhagwat2016', 400)
+# Hibiki2001
+Hibiki2001_data = pd.DataFrame({'void_fraction_Hibiki2001': [0.049, 0.092, 0.192], 'vg_Hibiki2001': [0.561, 0.642, 0.672]})
+numerical_data2 =pd.DataFrame( {'void_fraction_Hibiki2001': [0.049, 0.092, 0.192], 'vg_Hibiki2001': [0.605, 0.646, 0.635]})
+experimental_df=pd.concat([experimental_df,Hibiki2001_data])
+numerical_df=pd.concat([numerical_df,numerical_data2])
 
+plot_data_and_relative_error(experimental_df, numerical_df, {'Bhagwat2016','Hibiki2001'}, 400)
