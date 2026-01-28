51c1af21cfc99ba5a708705926f948d13f588838
MengnanLi91 <mengnanli91@gmail.com>
2026-01-27 21:45:38 -0700

    Fix naming inconsistence

--- test/tests/controls/moose_fmu/moose_fmu_tester.py
+++ test/tests/controls/moose_fmu/moose_fmu_tester.py
@@ -7,14 +7,16 @@
 # Licensed under LGPL 2.1, please see LICENSE for details
 # https://www.gnu.org/licenses/lgpl-2.1.html
 
-import os, sys
-from fmpy import simulate_fmu, extract, read_model_description, instantiate_fmu
-from fmpy.simulation import apply_start_values
+import logging
+import os
+import shutil
+import sys
+import time
+
 import numpy as np
 import pandas as pd
-import logging
-import time
-import shutil
+from fmpy import extract, instantiate_fmu, read_model_description, simulate_fmu
+from fmpy.simulation import apply_start_values
 
 
 # Helper for testing the MOOSE FMU via the RunApp command_proxy option
